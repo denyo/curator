cb1d9eed93ea88f4cb9bfb24ae3f82f2cabd430d
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-17 11:42:35 +0100

    ci: add build.sh to build docker container, upgrade base image to alpine:3.12.3

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE=denyo/elastic-curator
+TAG=$(cat curator/_version.py | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
+
+docker build -t $IMAGE:$TAG -t $IMAGE:latest .
+
+docker push $IMAGE:$TAG
+docker push $IMAGE:latest
