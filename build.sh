42781b107c54696f5bbaddea3884892dc391069a
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-18 10:16:47 +0100

    ci: build Dockerfile from pip package

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 IMAGE=denyo/elastic-curator
 TAG=$(cat curator/_version.py | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 
-docker build -t $IMAGE:$TAG -t $IMAGE:latest .
+docker build --build-arg VERSION=$TAG -t $IMAGE:$TAG -t $IMAGE:latest .
 
 docker push $IMAGE:$TAG
 docker push $IMAGE:latest
